5bb03e2ad4fe364d987213bbd0192ab7e2cacc37
NekoMimiOfficial <nekomimi@tilde.team>
2024-12-24 15:00:23 +0300

    v0.0.4 b0x020002 cleanup phase 2

---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+rm -r $HOME/.local/share/Bob/
+rm $HOME/.local/bin/bob
+echo "Uninstallation completed, if not used by other apps you could uninstall the NekoMimi python module by running [pip3 uninstall nekomimi]"
